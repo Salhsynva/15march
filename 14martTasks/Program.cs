780261e86866578d37aeb09b252448432646f30f
Salhsynva <100703384+Salhsynva@users.noreply.github.com>
2022-03-15 08:29:47 +0300

    Add files via upload

---
+++ 14martTasks/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _14martTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*int[] numbers = { 13, 42, 78, 99, 19 };       TASK1
+            int sum;
+            int residue;
+            int indexNumber;
+            int count = 0;
+            for(int i = 0;i < numbers.Length; i++)
+            {
+                sum = 0;
+               while(numbers[i] > 0)
+                {
+                    indexNumber = numbers[i];
+                    residue = indexNumber % 10;
+                    sum += residue;
+                    indexNumber = (indexNumber - residue) / 10;
+                   
+                }
+                if (sum < 10)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);*/
+
+
+
+
+            /*int count = 0;                 //TASK2
+            for(int i = 1; i <20; i++)
+             {
+                 if (i % 2 == 0)
+                 {
+                     count++;
+                 }
+             }
+             Console.WriteLine(count); */
+
+
+
+            /*int number = 25 ;              //TASK3
+            int i = 1;
+            if (number < 0)
+                Console.WriteLine("menfi eded daxil etmeyin!");
+            else if(number == 1)
+                Console.WriteLine("1");
+            else
+            {
+                while(i*i <= number)
+                {
+                    i++;
+                }
+            Console.WriteLine(i-1);
+            }*/
+
+
+        }
+    }
+}
